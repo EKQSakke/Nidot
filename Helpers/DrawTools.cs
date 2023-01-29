316c4ccb30e549189f0dfcf8ad3f73cfbe44caf6
Sakari Ekqvist <sakari.ekqvist@magicad.com>
2023-01-29 19:41:13 +0200

    Made usable with no previous mesh

--- Helpers/DrawTools.cs
+++ Helpers/DrawTools.cs
@@ -7,7 +7,7 @@ public static class DrawTools
     public static void DrawRay(MeshInstance3D rayMesh, Vector3 start, Vector3 end)
     {
         rayMesh.GlobalPosition = start;
-        var immediateMesh = (ImmediateMesh) rayMesh.Mesh;
+        var immediateMesh = new ImmediateMesh();
         immediateMesh.ClearSurfaces();
         immediateMesh.SurfaceBegin(Mesh.PrimitiveType.Lines, rayMesh.MaterialOverride);
         immediateMesh.SurfaceAddVertex(new Vector3(0, 0, 0));
