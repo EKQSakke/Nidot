1927cf29c0d192720d82145e0141728cc82a7ba0
Sakari Ekqvist <ekq.sakke@gmail.com>
2023-07-27 20:31:36 +0300

    List extensions

--- Extensions/NodeExtensions.cs
+++ Extensions/NodeExtensions.cs
@@ -74,3 +74,28 @@ public static class NodeExtensions
         return child;
     }
 }
+
+public static class ListExtensions
+{
+    public static bool TryAdd<T>(this List<T> list, T item)
+    {
+        if (!list.Contains(item))
+        {
+            list.Add(item);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool TryRemove<T>(this List<T> list, T item)
+    {
+        if (list.Contains(item))
+        {
+            list.Remove(item);
+            return true;
+        }
+
+        return false;
+    }
+}
