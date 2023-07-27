4495fe1b7d7ab72420900f15f18f2edfe0991fb9
Sakari Ekqvist <ekq.sakke@gmail.com>
2023-07-27 20:32:00 +0300

    ListExtensions

---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Nidot;
+
+public static class ListExtensions
+{
+    public static bool TryAdd<T>(this List<T> list, T item)
+    {
+        if (!list.Contains(item))
+        {
+            list.Add(item);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool TryRemove<T>(this List<T> list, T item)
+    {
+        if (list.Contains(item))
+        {
+            list.Remove(item);
+            return true;
+        }
+
+        return false;
+    }
+}
